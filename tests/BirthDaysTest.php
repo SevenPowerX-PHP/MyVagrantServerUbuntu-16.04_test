04a3f7142665cd9f3ab1c8a3b40c6b1df51e7b6c
Andrii Lavryniuk <splaandrey@gmail.com>
2018-09-24 20:34:43 +0000

    Added composer.lock

---
+++ tests/BirthDaysTest.php
@@ -0,0 +1,15 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: splaa
+	 * Date: 24.09.18
+	 * Time: 21:43
+	 */
+	
+	namespace Splx\Dates;
+	
+	
+	class BirthDaysTest extends \PHPUnit_Framework_TestCase
+	{
+		
+	}
